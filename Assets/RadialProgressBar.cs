3a2037c556fa738c92819fb652f2dd9b9cdc902d
Rikharthu <uberviolence@gmail.com>
2017-03-13 21:38:36 +0200

    First commit

---
+++ Assets/RadialProgressBar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class RadialProgressBar : MonoBehaviour
+{
+
+    public Transform LoadingBar;
+    public Transform TextIndicator;
+    public Transform TextLoading;
+    [SerializeField] private float currentAmount;
+    [SerializeField] private float speed;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentAmount < 100)
+        {
+            // Loading...
+			currentAmount+=speed*Time.deltaTime;
+			TextIndicator.GetComponent<Text>().text=((int)currentAmount).ToString()+"%";
+			TextLoading.gameObject.SetActive(true);
+        }
+        else
+        {
+            // Done
+			TextLoading.gameObject.SetActive(false);
+			TextIndicator.GetComponent<Text>().text = "DONE!";
+        }
+
+		LoadingBar.GetComponent<Image>().fillAmount = currentAmount/100;
+    }
+}
